33d7affdf2f0a2a7200589356d64a1e8adb0dce6
eziztm <eziztm@yahoo.com>
2018-05-22 15:52:18 +0200

    refactor BrregHealthIndicator

--- app/src/main/java/no/nav/pam/ad/enhetsregister/BrregHealthIndicator.java
+++ app/src/main/java/no/nav/pam/ad/enhetsregister/BrregHealthIndicator.java
@@ -1,9 +1,9 @@
 package no.nav.pam.ad.enhetsregister;
 
+import no.nav.pam.ad.enhetsregister.batch.BatchConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -18,25 +18,29 @@ public class BrregHealthIndicator implements HealthIndicator {
 
     private static final Logger LOG = LoggerFactory.getLogger(BrregHealthIndicator.class);
 
-    @Value("${pam.enhetsregister.sources.underenhet.url:http://data.brreg.no/enhetsregisteret/download/underenheter}")
-    private String enhetsregisterUnderenhetUrl;
-
+    private final BatchConfig.Underenhet underenhet;
     private final Proxy proxy;
 
     @Autowired
-    public BrregHealthIndicator(Proxy proxy) {
+    public BrregHealthIndicator(BatchConfig.Underenhet underenhet, Proxy proxy) {
+        this.underenhet = underenhet;
         this.proxy = proxy;
     }
 
+
     @Override
     public Health health() {
+        if(!underenhet.getUrl().isPresent()){
+            return Health.down().build();
+        }
+
         try {
-            URL url = new URL(enhetsregisterUnderenhetUrl);
+            URL url = underenhet.getUrl().get();
             HttpURLConnection connection = (HttpURLConnection) url.openConnection(proxy);
             connection.setRequestMethod("HEAD");
             return (connection.getResponseCode() == 200) ? Health.up().build() : Health.down().build();
         } catch (IOException e) {
-            LOG.error("Failed health check for url {}", enhetsregisterUnderenhetUrl, e);
+            LOG.error("Failed health check for url {}", underenhet.getUrl().get(), e);
             return Health.down(e).build();
         }
     }

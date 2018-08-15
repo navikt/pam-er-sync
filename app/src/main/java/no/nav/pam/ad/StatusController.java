cd41223538c571c48d3d6611156aa5fcc6380bb6
Øyvind Stegard <oyvind.stegard@nav.no>
2018-08-15 09:50:26 +0200

    Log errors in StatusController

--- app/src/main/java/no/nav/pam/ad/StatusController.java
+++ app/src/main/java/no/nav/pam/ad/StatusController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import no.nav.pam.ad.enhetsregister.BrregHealthIndicator;
 import no.nav.pam.ad.es.IndexClientHealthIndicator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-
 @Controller
 @RequestMapping("/internal/status")
 public class StatusController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StatusController.class);
 
     private final IndexClientHealthIndicator indexClientHealthIndicator;
     private final BrregHealthIndicator brregHealthIndicator;
@@ -26,19 +28,21 @@ public class StatusController {
         this.brregHealthIndicator = brregHealthIndicator;
     }
 
-    @Autowired
-
     @GetMapping
-    public ResponseEntity<ObjectNode> statusHealth() throws IOException {
+    public ResponseEntity<ObjectNode> statusHealth() throws Exception {
+        try {
 
-        boolean isElastisSearchOK = indexClientHealthIndicator.health().getStatus().equals(Status.UP);
-        boolean isBrregOk = brregHealthIndicator.health().getStatus().equals(Status.UP);
+            boolean isElastisSearchOK = indexClientHealthIndicator.health().getStatus().equals(Status.UP);
+            boolean isBrregOk = brregHealthIndicator.health().getStatus().equals(Status.UP);
 
+            ObjectNode node = JsonNodeFactory.instance.objectNode();
+            node.put("Elastic Search connection status", (isElastisSearchOK) ? "OK" : "NOT OK");
+            node.put("BRREG connection status", (isBrregOk) ? "OK" : "NOT OK");
 
-        ObjectNode node = JsonNodeFactory.instance.objectNode();
-        node.put("Elastic Search connection status", (isElastisSearchOK) ? "OK" : "NOT OK");
-        node.put("BRREG connection status", (isBrregOk) ? "OK" : "NOT OK");
-
-        return ResponseEntity.ok(node);
+            return ResponseEntity.ok(node);
+        } catch (Exception e) {
+            LOG.error("Status check failed", e);
+            throw e;
+        }
     }
 }

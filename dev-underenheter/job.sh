b68e60f2431d7da7557c547167e11113c3d9aca8
noraness <nora.storebo.ness@nav.no>
2023-01-09 09:01:02 +0100

    Post i stedet for put samt pretty=true mot ESkall.

--- dev-underenheter/job.sh
+++ dev-underenheter/job.sh
@@ -17,7 +17,7 @@ sleep 10
 
 for doc in /data/*.json; do
     orgnr=$(basename $doc .json)
-    curl -sS -u "$ES_USER:$ES_PASSWORD" -XPUT -H 'Content-type: application/json' --data-binary @"$doc" "$ES_STILLING_BACKEND/underenhet/_doc/$orgnr"
+    curl -sS -u "$ES_USER:$ES_PASSWORD" -XPOST -H 'Content-type: application/json' --data-binary @"$doc" "$ES_STILLING_BACKEND/underenhet/_doc/$orgnr?pretty=true"
     echo
     echo "Indexed dev-underenhet $orgnr"
 done

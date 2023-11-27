cd35a14d9b97319d6df0684ec6abb1967ab711f0
noraness <nora.storebo.ness@nav.no>
2023-11-27 18:20:55 +0100

    Remove unused imports and comment

--- src/test/java/no/nav/pam/ad/enhetsregister/batch/BatchJobTest.java
+++ src/test/java/no/nav/pam/ad/enhetsregister/batch/BatchJobTest.java
@@ -9,15 +9,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.batch.core.*;
 import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashMap;
--- src/test/java/no/nav/pam/ad/enhetsregister/batch/TestBatchConfig.java
+++ src/test/java/no/nav/pam/ad/enhetsregister/batch/TestBatchConfig.java
@@ -1,51 +1,14 @@
 package no.nav.pam.ad.enhetsregister.batch;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import no.nav.pam.ad.enhetsregister.model.Enhet;
-import no.nav.pam.ad.enhetsregister.model.reader.ReaderEnhet;
-import no.nav.pam.ad.es.IndexService;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.json.JacksonJsonObjectReader;
-import org.springframework.batch.item.json.JsonItemReader;
-import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
-import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Optional;
-import java.util.zip.GZIPInputStream;
-
-import static no.nav.pam.ad.enhetsregister.batch.JobLauncherService.*;
 
 @TestConfiguration
 public class TestBatchConfig extends DefaultBatchConfiguration {
@@ -69,18 +32,5 @@ public class TestBatchConfig extends DefaultBatchConfiguration {
     public DataSource getDataSource() {
         return primaryDataSource();
     }
-
-    /*
-    @Bean
-    JobLauncherTestUtils jobLauncherTestUtils(JobRepository jobRepository, JobLauncher jobLauncher, Job importUserJob) {
-        JobLauncherTestUtils jobLauncherTestUtils1 = new JobLauncherTestUtils();
-        jobLauncherTestUtils1.setJob(importUserJob);
-        jobLauncherTestUtils1.setJobRepository(jobRepository);
-        jobLauncherTestUtils1.setJobLauncher(jobLauncher);
-        return jobLauncherTestUtils1;
-    }
-
-     */
-
 
 }

356e5d0c5721b7053e4575ad0928223ce0664b26
noraness <nora.storebo.ness@nav.no>
2023-11-27 12:03:33 +0100

    Fix indextest

--- src/test/java/no/nav/pam/ad/enhetsregister/es/IndexServiceTest.java
+++ src/test/java/no/nav/pam/ad/enhetsregister/es/IndexServiceTest.java
@@ -5,16 +5,19 @@ import no.nav.pam.ad.es.Datestamp;
 import no.nav.pam.ad.es.IndexClient;
 import no.nav.pam.ad.es.IndexService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,6 +26,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class IndexServiceTest {
 
     @Mock
@@ -32,9 +36,9 @@ public class IndexServiceTest {
     private IndexService service;
 
     @Captor
-    private ArgumentCaptor<String> captor;
+    private ArgumentCaptor<String[]> captor;
 
-/*
+
     @Test
     public void shouldDeleteOlderIndices() throws IOException {
         String expired1 = LocalDateTime.now().minusDays(12).format(DateTimeFormatter.ofPattern(Datestamp.DATESTAMP_FORMAT));
@@ -52,12 +56,12 @@ public class IndexServiceTest {
         service.deleteOlderIndices(DataSet.UNDERENHET.toString());
 
         verify(client).deleteIndex(captor.capture());
-        List<String> args = captor.getAllValues();
+        String[] args = captor.getValue();
 
-        assertEquals(2, args.size());
-        assertTrue(args.stream().anyMatch(s -> s.equals(expired1)));
-        assertTrue(args.stream().anyMatch(s -> s.equals(expired2)));
+        assertEquals(2, args.length);
+        assertTrue(Arrays.stream(args).anyMatch(s -> s.equals(expired1)));
+        assertTrue(Arrays.stream(args).anyMatch(s -> s.equals(expired2)));
     }
 
- */
+
 }

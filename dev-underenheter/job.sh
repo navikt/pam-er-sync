077eadd6162f945ab907df180045f511da1137d7
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2025-04-28 15:43:54 +0200

    Fjern venting på Linkerd
    Nais bruker ikke lengre Linkerd, så dette steget kan fjernet. Det feiler uansett bare i loggene.

--- dev-underenheter/job.sh
+++ dev-underenheter/job.sh
@@ -9,12 +9,6 @@ coproc JSON_LOGGER { jq --unbuffered -Rc '{"@timestamp": now|strftime("%Y-%m-%dT
 exec 1>&${JSON_LOGGER[1]} 2>&${JSON_LOGGER[1]}
 # End setup JSON logging
 
-# Shutdown linkerd on exit, to ensure clean and timely pod shutdown
-trap 'echo Shutdown linkerd: "$(curl -sS -XPOST http://127.0.0.1:4191/shutdown 2>&1)"' EXIT
-
-echo 'Waiting 10 seconds for linkerd ..'
-sleep 10
-
 for doc in /data/*.json; do
     orgnr=$(basename $doc .json)
     curl -sS -u "$ES_USER:$ES_PASSWORD" -XPOST -H 'Content-type: application/json' --data-binary @"$doc" "$ES_STILLING_BACKEND/underenhet/_doc/$orgnr?pretty=true"
